749cdb50fec962d4651b8d6a44dc9eb6357b036c
Rubinelle <rubinelle@gmail.com>
2022-10-20 20:53:18 -0300

    testando commit

--- src/br/com/dio/calculadora/Calculadora.java
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -7,7 +7,7 @@ public class Calculadora {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 
-		int a, b;
+		int a, b; 
 
 		System.out.println("Digite o primeiro valor: ");
 		a = scan.nextInt();
